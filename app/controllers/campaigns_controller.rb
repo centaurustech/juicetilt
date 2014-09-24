f101541e97a173a39bc9fe05fa14cd5d9b41c11c
gxespino <glennpeter.espinosa@gmail.com>
2014-09-24 14:38:37 -0400

    delete show_all_perks function cause it didn't work

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -40,10 +40,6 @@ class CampaignsController < ApplicationController
     redirect_to campaigns_url, notice: 'Campaign was successfully destroyed.'    
   end
 
-  def show_campaign_perks
-    @perks = Perk.all
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_campaign
